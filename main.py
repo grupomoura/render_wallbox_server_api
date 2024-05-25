f968d16808063a0de84c32e0372a76c307635fc8
James Moura <35638433+grupomoura@users.noreply.github.com>
2024-05-25 16:43:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -211,4 +211,4 @@ async def set_charger_schedules(charger_id: int, schedules: List[ChargerSchedule
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000 reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
